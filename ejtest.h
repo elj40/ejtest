23fdd29a6eee69bfcb3968cef49cf28be4618d6a
elj40 <elaijoubert@gmail.com>
2025-05-27 22:35:56 +0200

    Add COLOURS!!!!
    Adds the ansi escape codes to ejtest_print_result to output green on
    success and red on failure.
    Not checking if environment even supports the colours tho.
    Now can get even more dopamine on pass

--- ejtest.h
+++ ejtest.h
@@ -1,7 +1,7 @@
 #ifndef EJ_TEST_H
 #define EJ_TEST_H
 
-#define EJ_TEST_VERSION 1.0.0
+#define EJ_TEST_VERSION 1.0.1
 
 #include <stdio.h>
 #include <stdbool.h>
@@ -13,8 +13,23 @@
 #define ejtest_expect_float(p, a, b) ejtest_expect_float_file_line((p), (a), (b), __FILE__, __LINE__)
 #define ejtest_expect_struct(p, a, b, cmp) ejtest_expect_struct_file_line((p), &(a), &(b), sizeof(a), sizeof(b), (cmp), __FILE__, __LINE__)
 
-#define ejtest_print_result(name, result) printf("== %-30s: %s ==\n", (name), (result) ? "success" : "FAILURE");
+//==============================================
+// I got the following from RabaDabaDoba at
+// https://gist.github.com/RabaDabaDoba/145049536f815903c79944599c6f952a
+// I just added the EJTEST_... prefix to not annoy users if they need the names
+#define EJTEST_RED "\e[0;31m"
+#define EJTEST_GRN "\e[0;32m"
+#define EJTEST_YEL "\e[0;33m"
+#define EJTEST_CRESET "\e[0m"
+//==============================================
 
+void ejtest_print_result(const char * name, bool result)
+{
+    const char * colour = (result) ? EJTEST_GRN : EJTEST_RED;
+    printf("%s== ", colour);
+    printf("%-30s", name);
+    printf("%s: %s =="EJTEST_CRESET"\n", colour, (result) ? "success" : "FAILURE");
+};
 bool ejtest_nearly_equal(float a, float b)
 {
 #define EJ_TEST_EPSILON (1.0/4096.0)
@@ -22,50 +37,55 @@ bool ejtest_nearly_equal(float a, float b)
     else if (b > a && b - a > EJ_TEST_EPSILON) return false;
     return true;
 }
-void ejtest_expect_bool_file_line(bool * p, bool a, bool b, const char * file, int line)
+bool ejtest_expect_bool_file_line(bool * p, bool a, bool b, const char * file, int line)
 {
     if (a == b) {
         *p = *p && true;
-        return;
-    }
-    printf("%s,%d:: Expected equal: |%d|; |%d|\n", file, line, a, b);
-    *p = *p && false;
-}
-void ejtest_expect_int_file_line(bool * p, int a, int b, const char * file, int line)
-{
-    if (a == b) {
-        *p = *p && true;
-        return;
+        return *p;
     }
     printf("%s,%d:: Expected equal: |%d|; |%d|\n", file, line, a, b);
     *p = false;
+    return *p;
 }
-
-void ejtest_expect_char_file_line(bool * p, char a, char b, const char * file, int line)
+bool ejtest_expect_int_file_line(bool * p, int a, int b, const char * file, int line)
 {
     if (a == b) {
         *p = *p && true;
-        return;
+        return *p;
+    }
+    printf("%s,%d:: Expected equal: |%d|; |%d|\n", file, line, a, b);
+    *p = false;
+    return *p;
+}
+
+bool ejtest_expect_char_file_line(bool * p, char a, char b, const char * file, int line)
+{
+    if (a == b) {
+        *p = *p && true;
+        return *p;
     }
     printf("%s,%d:: Expected equal: |%c|; |%c|\n", file, line, a, b);
     *p = false;
+    return *p;
 }
-void ejtest_expect_float_file_line(bool * p, float a, float b, const char * file, int line)
+bool ejtest_expect_float_file_line(bool * p, float a, float b, const char * file, int line)
 {
     if (ejtest_nearly_equal(a, b)) {
         *p = *p && true;
-        return;
+        return *p;
     }
     printf("%s,%d:: Expected nearly equal: |%f|; |%f|\n", file, line, a, b);
     *p  = false;
+    return *p;
 }
-void ejtest_expect_struct_file_line(bool * p, void * a, void * b, int a_size, int b_size, bool (*compare)(void *, void*), const char * file, int line)
+bool ejtest_expect_struct_file_line(bool * p, void * a, void * b, int a_size, int b_size, bool (*compare)(void *, void*), const char * file, int line)
 {
    if (a_size == b_size && (*compare)(a, b)) {
        *p = *p && true;
-       return;
+       return *p;
    }
    printf("%s,%d:: Expected equal structs\n", file, line);
    *p = false;
+   return *p;
 };
 #endif // EJ_TEST_H
